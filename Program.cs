0d774ee387b27a34a22f7e77ccaec36ba1d29d58
singhvishal14 <60248427+singhvishal14@users.noreply.github.com>
2023-08-08 11:39:50 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,67 +1,112 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace PlayerAndTeamRequirements
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
             CricketTeam team = new CricketTeam();
 
-            MainMenu:
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1. Add Player");
-            Console.WriteLine("2. Remove Player");
-            Console.WriteLine("3. Get Player by Id");
-            Console.WriteLine("4. Get Players by Name");
-            Console.WriteLine("5. Get All Players");
-            Console.WriteLine("6. Exit");
-
-            if (int.TryParse(Console.ReadLine(), out int choice))
+            while (true)
             {
-                switch (choice)
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("Press 1 for  Add Player");
+                Console.WriteLine("Press 2 Remove Player");
+                Console.WriteLine("Press 3 Get Player by Id");
+                Console.WriteLine("Press 4 Get Players by Name");
+                Console.WriteLine("Press 5. Get All Players");
+                Console.WriteLine("6. Exit");
+
+                if (int.TryParse(Console.ReadLine(), out int choice))
                 {
-                    case 1:
-                        Console.WriteLine("Please Enter Player Id:");
-                        int playerId = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Please Enter Player Id:");
+                            int playerId = int.Parse(Console.ReadLine());
 
-                        Console.WriteLine("Please Enter Player Name:");
-                        string playerName = Console.ReadLine();
+                            Console.WriteLine("Please Enter Player Name:");
+                            string playerName = Console.ReadLine();
 
-                        Console.WriteLine("Please Enter Player Age:");
-                        int playerAge = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Please Enter Player Age:");
+                            int playerAge = int.Parse(Console.ReadLine());
 
-                        team.AddPlayer(new Player { Id = playerId, Name = playerName, Age = playerAge });
-                        goto MainMenu;
+                            team.AddPlayer(new Player { Id = playerId, Name = playerName, Age = playerAge });
+                            break;
 
-                    case 2:
-                        Console.WriteLine("Please Enter Player Id to remove:");
-                        int playerIdToRemove = int.Parse(Console.ReadLine());
+                        case 2:
+                            Console.WriteLine("Please Enter Player Id to remove:");
+                            int playerIdToRemove = int.Parse(Console.ReadLine());
 
-                        team.RemovePlayer(playerIdToRemove);
-                        goto MainMenu;
+                            team.RemovePlayer(playerIdToRemove);
+                            break;
 
-                    case 3:
-                        // ... Similar for other cases ...
-                        
-                    case 6:
-                        Console.WriteLine("Exiting the program.");
-                        Environment.Exit(0);
-                        break;
+                        case 3:
+                            Console.WriteLine("Please Enter Player Id to get details:");
+                            int playerIdToGet = int.Parse(Console.ReadLine());
 
-                    default:
-                        Console.WriteLine("Invalid choice. Please try again.");
-                        goto MainMenu;
+                            Player playerById = team.GetPlayerById(playerIdToGet);
+                            if (playerById != null)
+                            {
+                                Console.WriteLine($"Player Found: Id: {playerById.Id}, Name: {playerById.Name}, Age: {playerById.Age}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Player not found.");
+                            }
+                            break;
+
+                        case 4:
+                            Console.WriteLine("Please Enter Player Name to get details:");
+                            string playerNameToGet = Console.ReadLine();
+
+                            List<Player> playersByName = team.GetPlayersByName(playerNameToGet);
+                            if (playersByName.Count > 0)
+                            {
+                                Console.WriteLine($"Players Found with name '{playerNameToGet}':");
+                                foreach (Player player in playersByName)
+                                {
+                                    Console.WriteLine($"Id: {player.Id}, Name: {player.Name}, Age: {player.Age}");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine($"No players found with name '{playerNameToGet}'.");
+                            }
+                            break;
+
+                        case 5:
+                            List<Player> allPlayers = team.GetAllPlayers();
+                            Console.WriteLine("All Players:");
+                            foreach (Player player in allPlayers)
+                            {
+                                Console.WriteLine($"Id: {player.Id}, Name: {player.Name}, Age: {player.Age}");
+                            }
+                            break;
+
+                        case 6:
+                            Console.WriteLine("Exiting the program.");
+                            Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid choice. Please try again.");
+                            break;
+                    }
                 }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number.");
-                goto MainMenu;
-            }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
 
-            Console.WriteLine();
+                Console.WriteLine();
+            }
         }
     }
 }
